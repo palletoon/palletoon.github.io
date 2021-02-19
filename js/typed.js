5a68bba6643dd588f8924e2d3839d0adef9c8e1a
palletoon <palletoon@gmail.com>
2021-02-19 11:53:21 -0500

    changed intro

--- js/typed.js
+++ js/typed.js
@@ -396,7 +396,7 @@
     };
 
     $.fn.typed.defaults = {
-        strings: ["These are the default values...", "You know what you should do?", "Use your own!", "Have a great day!"],
+        strings: ["My name is Seri B.", "I'm studying speech & language pathology", "and TESOL", "Have a great day!"],
         stringsElement: null,
         // typing speed
         typeSpeed: 0,
